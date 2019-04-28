123c6ac727a836fe2dd4b0dc533c6ccd1c526507
anhbaysgalan1 <anhbaysgalan1@gmail.com>
2019-04-28 21:20:19 +0800

    create server

---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  AUTH0_DOMAIN: 'dev-9wfy7c0y.auth0.com', // e.g., kmaida.auth0.com
+  AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://anhbaysgalan:12072119Aa.@ds255265.mlab.com:55265/mean',
+  NAMESPACE: 'http://localhost:8083/roles' // e.g., http://myapp.com/roles
+};
