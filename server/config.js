79dc5e65e7295c51b99addb0b6553b4395cc350f
anhaaD <haveman27@gmail.com>
2019-05-06 04:18:58 +0800

    replace server

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   AUTH0_DOMAIN: 'dev-9wfy7c0y.auth0.com', // e.g., kmaida.auth0.com
-  AUTH0_API_AUDIENCE: 'https://auth-server-mean.appspot.com/api/', // e.g., 'http://localhost:8083/api/'
+  AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
   MONGO_URI: process.env.MONGO_URI || 'mongodb://anhbaysgalan:12072119Aa.@ds255265.mlab.com:55265/mean',
-  NAMESPACE: 'https://auth-server-mean.appspot.com.appspot.com/roles' // e.g., http://myapp.com/roles
+  NAMESPACE: 'http://localhost:8083/roles' // e.g., http://myapp.com/roles
 };
